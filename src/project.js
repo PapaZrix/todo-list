5d5fee2bf47e05111bac112ba8de43ded2b06767
PapaZrix <zrilic313@gmail.com>
2023-01-13 12:14:07 +0100

    Add project and task class modules

---
+++ src/project.js
@@ -0,0 +1,36 @@
+import Task from "./task"
+
+export default class Project {
+    constructor(name) {
+        this.name = name
+        this.tasks = []
+    }
+
+    setName(name) {
+        this.name = name
+    }
+
+    getName() {
+        return this.name
+    }
+
+    setTasks(tasks) {
+        this.tasks = tasks
+    }
+
+    getTasks() {
+        return this.tasks
+    }
+
+    getTask(taskName) {
+        return this.tasks.find((task) => task.getName() === taskName)
+    }
+
+    addTask(newTask, date) {
+        this.tasks.push(new Task(newTask, date))
+    }
+
+    deleteTask(index) {
+        this.tasks.splice(index, 1)
+    }
+}
