f79b34861ee97915eef3b9d11f0c96090250b1d8
PapaZrix <zrilic313@gmail.com>
2023-01-16 14:15:57 +0100

    Add logic for adding and rendering tasks to a project

--- src/UI.js
+++ src/UI.js
@@ -12,13 +12,21 @@ export const UI = (() => {
     storage.setCurrentProject(0);
     console.log(storage.getCurrentProject());
 
-    // LISTENERS
+    console.log(storage.getTodoList().getProject("Inbox").getTasks());
+
+    // TASK LISTENERS
     const initTaskButtons = () => {
         const addTaskBtn = document.querySelector(".add-task-btn");
         const cancelTaskBtn = document.querySelector(".cancel-btn");
+        const taskForm = document.querySelector("#task-form");
 
         addTaskBtn.addEventListener("click", openAddTask);
         cancelTaskBtn.addEventListener("click", closeAddTask);
+        taskForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+            addToTasks();
+            renderTasks();
+        });
     }
 
     const openAddTask = () => {
@@ -41,6 +49,33 @@ export const UI = (() => {
         taskForm.reset();
     }
 
+    const addToTasks = () => {
+        const projectName = document.querySelector(".project-name").textContent;
+        const name = document.querySelector("#name").value;
+        const date = document.querySelector("#date").value;
+
+        storage.addTask(projectName, name, date);
+        closeAddTask();
+    }
+
+    const renderTasks = () => {
+        const projectName = document.querySelector(".project-name").textContent;
+        const taskList = document.querySelector(".task-list");
+        taskList.innerHTML = "";
+
+        storage.getTodoList().getProject(projectName).getTasks().forEach((task, index) => {
+            taskList.innerHTML += `
+                <button class="task" id=${index}>
+                         <i class="fa-solid fa-list-check"></i>
+                         <p class="task-name">${task.name}</p>
+                         <p class="task-date">${task.dueDate}</p>
+                         <i class="fa-solid fa-pen-to-square"></i>
+                         <i class="fa-solid fa-x"></i>
+                 </button>`
+        })
+    }
+
     initTaskButtons();
+    renderTasks();
 
 })();
