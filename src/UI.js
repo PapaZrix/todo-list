70651c9a5ec581e15fa8238b45a368ada3f5caee
PapaZrix <zrilic313@gmail.com>
2023-01-16 14:56:23 +0100

    Add functions to delete and edit tasks

--- src/UI.js
+++ src/UI.js
@@ -19,14 +19,12 @@ export const UI = (() => {
         const addTaskBtn = document.querySelector(".add-task-btn");
         const cancelTaskBtn = document.querySelector(".cancel-btn");
         const taskForm = document.querySelector("#task-form");
+        const taskList = document.querySelector(".task-list");
 
         addTaskBtn.addEventListener("click", openAddTask);
         cancelTaskBtn.addEventListener("click", closeAddTask);
-        taskForm.addEventListener("submit", (e) => {
-            e.preventDefault();
-            addToTasks();
-            renderTasks();
-        });
+        taskForm.addEventListener("submit", addTask);
+        taskList.addEventListener("click", handleTask);
     }
 
     const openAddTask = () => {
@@ -49,13 +47,61 @@ export const UI = (() => {
         taskForm.reset();
     }
 
-    const addToTasks = () => {
+    const addTask = (e) => {
+        e.preventDefault();
         const projectName = document.querySelector(".project-name").textContent;
         const name = document.querySelector("#name").value;
         const date = document.querySelector("#date").value;
+        const submitBtn = document.querySelector(".submit-btn");
+        const taskForm = document.querySelector("#task-form");
 
-        storage.addTask(projectName, name, date);
+        let projectTaskList = storage.getTodoList().getProject(projectName).getTasks();
+        const isDuplicate = projectTaskList.some((task) => task.name.toUpperCase() === name.toUpperCase());
+        
+        if (submitBtn.textContent === "Add") {
+            if (isDuplicate) {
+                alert("Tasks must have different names!");
+                return
+            }
+            storage.addTask(projectName, name, date);            
+        } else {
+            const taskId = storage.getCurrentTask();
+            const newName = taskForm.elements.taskName.value;
+            const newDate = taskForm.elements.dueDate.value;
+            storage.renameTask(projectName, taskId, newName);
+            storage.setTaskDate(projectName, taskId, newDate);
+            storage.resetCurrentTask();
+        }
+
         closeAddTask();
+        renderTasks();
+    }
+
+    const editTask = (index) => {
+        const name = document.querySelector("#name");
+        const date = document.querySelector("#date");
+
+        name.value = storage.getTodoList().getProjects()[storage.getCurrentProject()].getTasks()[index].name;
+        date.value = storage.getTodoList().getProjects()[storage.getCurrentProject()].getTasks()[index].dueDate;
+    }
+
+    const handleTask = (e) => {
+        storage.setCurrentTask(storage.getCurrentProject(), e.target.parentNode.id);
+        console.log(storage.getCurrentTask());
+
+        if (e.target.classList.contains("fa-x")) {
+            const projectName = document.querySelector(".project-name").textContent;
+
+            storage.deleteTask(projectName, e.target.parentNode.id);
+            renderTasks();
+        }
+        if (e.target.classList.contains("fa-pen-to-square")) {            
+            const submitBtn = document.querySelector(".submit-btn");
+
+            editTask(e.target.parentNode.id);
+            openAddTask();
+            submitBtn.textContent = "Edit";
+        }
     }
 
     const renderTasks = () => {
